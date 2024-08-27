d6197870473e905e8bfae0c368769a894442794a
BrittneysProjects <brittneysinternship@gmail.com>
2024-08-27 13:36:13 -0400

    add.py
    Updated script to fix path handling and improve robustness

--- scripts/add.py
+++ scripts/add.py
@@ -1,36 +1,31 @@
-#https: // github.com / DataGlacier / VC.git
-import json, os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import json
+import os
 
+# Get the directory of the current script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# User can add name and favourite sport in response.json
-# default sport Cricket will be added incase user does not provide fav sport
-
-
+# Function to load the JSON data
 def load_json():
-    with open('../response.json') as json_obj:
+    # Use an absolute path to open the file
+    with open(os.path.join(BASE_DIR, '../response.json')) as json_obj:
         response = json.load(json_obj)
     return response
 
+# Function to write data to the JSON file
+def write_json(data, filename=os.path.join(BASE_DIR, '../response.json')):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=0)
 
-response = load_json()
-
-def write_json(data,filename = '../response.json'):
-    with open(filename,'w') as file:
-        json.dump(data,file,indent=0)
-
-
+# Function to prompt user for name and favorite sport
 def call_sport():
     name = input("Please add your name: ")
     sport = input("Please add your favourite sports name: ")
-    if (sport == ""):
+    if sport == "":
         sport = 'Cricket'
-    if (name):
+    if name:
         response[name] = sport
         write_json(response)
 
-
 if __name__ == "__main__":
+    response = load_json()  # Load JSON only when the script is executed directly
     call_sport()
-
-call_sport()
