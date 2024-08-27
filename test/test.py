477bf0969180046e135344ad1df138f85bea5645
BrittneysProjects <brittneysinternship@gmail.com>
2024-08-27 15:10:12 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,10 +1,20 @@
+import json
+import sys
+import os
 
-import sys,os
-sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+# Add the parent directory to the system path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from scripts.add import load_json
 
+def add_function(json_obj):
+    response = json.load(json_obj)
+    # ... rest of your code
+
+def save_data(data, file):
+    json.dump(data, file, indent=0)
 
 def test_load_json():
-  response = load_json()
-  assert isinstance(response, dict)
-  assert response.get("Data Glacier") =="Cricket"
+    response = load_json()
+    assert isinstance(response, dict)
+    assert response.get("Data Glacier") == "Cricket"
